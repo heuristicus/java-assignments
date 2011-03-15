c386a7929131b1563801bb23ca721a1c4cd6b52e
Michal Staniaszek <m.staniaszek@gmail.com>
2011-03-15 21:28:58 +0000

    more gradual class building, nothing significant yet

--- uni/SSC2/threadLibrary/Server/LibServer.java
+++ uni/SSC2/threadLibrary/Server/LibServer.java
@@ -5,6 +5,8 @@
 
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 
 /**
@@ -14,16 +16,34 @@ import java.util.ArrayList;
 public class LibServer {
 
     int port;
+    ServerSocket servSock;
     ArrayList<LibServSocket> connections;
 
     public LibServer(int port, int maxConnections){
         this.port = port;
         connections = new ArrayList<LibServSocket>();
+        initServSock();
+    }
+
+    private void initServSock(){
+        try {
+            servSock = new ServerSocket(port);
+        } catch (IOException ex) {
+            System.out.println("Error while initialising server socket.");
+            ex.printStackTrace();
+        }
+
     }
 
     public void shutdown(){
         for (LibServSocket libServSocket : connections) {
             libServSocket.disconnect();
+        }
+        try {
+            servSock.close();
+        } catch (IOException ex) {
+            System.out.println("Error while attempting to close server socket.");
+            ex.printStackTrace();
         }
     }
 
--- uni/SSC2/threadLibrary/Server/LibSocketListener.java
+++ uni/SSC2/threadLibrary/Server/LibSocketListener.java
@@ -2,13 +2,32 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Server;
+
+import java.io.ObjectOutputStream;
 
 /**
  *
  * @author michal
  */
-public class LibSocketListener {
+public class LibSocketListener implements Runnable {
 
+    private final LibServer server;
+    private final ObjectOutputStream objOut;
+
+    public LibSocketListener(ObjectOutputStream objOut, LibServer server) {
+        this.objOut = objOut;
+        this.server = server;
+    }
+
+    public void listen(){
+        while (!Thread.interrupted()){
+            
+        }
+
+    }
+
+    public void run() {
+        listen();
+    }
 }
